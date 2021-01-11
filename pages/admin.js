2a808de5fad87802ab8f01027d38cc14c3a17945
rapsby <gsu5022@gmail.com>
2021-01-11 15:02:02 +0900

    add iframe

--- pages/admin.js
+++ pages/admin.js
@@ -4,6 +4,14 @@ export default function Admin() {
       <p>This is the admin page</p>
       <p>123</p>
       <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"></img>
+      <iframe
+        src="https://open.spotify.com/embed/playlist/3NGlOldupdt94cdoEb5n0O"
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
     </div>
   );
 }
