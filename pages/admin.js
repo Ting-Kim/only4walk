afc27269a544216805d4c2583cfbbe527fed89ea
rapsby <gsu5022@gmail.com>
2021-01-11 09:18:00 +0900

    test commit

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,9 @@
+const GMAPS_API_KEY = `GMAPS_API_KEY`
 export default function Admin(){
     return(
     <div>
     <p>This is the admin page</p>
+    <p>{GMAPS_API_KEY}</p>
     </div>
     );
     }
