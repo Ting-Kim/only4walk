1d531d3f5ad089f3678ce15ad5b2de5dae5bca36
rapsby <gsu5022@gmail.com>
2021-01-11 09:37:58 +0900

    change key name

--- pages/admin.js
+++ pages/admin.js
@@ -1,10 +1,10 @@
-const GMAPS_API_KEY = process.env.GMAPS_API_KEY;
+const GMAPS_API_KEY = process.env.KEY;
 export default function Admin() {
   return (
     <div>
       <p>This is the admin page</p>
       <p>123</p>
-      <p>{GMAPS_API_KEY}</p>
+      <p>{GMAPS_API_KEY.length}</p>
     </div>
   );
 }
