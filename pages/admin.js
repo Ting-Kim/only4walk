d2711b7454caf0256fc73cf95539e058f0f5ec73
rapsby <gsu5022@gmail.com>
2021-01-11 09:40:52 +0900

    alength

--- pages/admin.js
+++ pages/admin.js
@@ -1,10 +1,11 @@
 const GMAPS_API_KEY = process.env.KEY;
+const alength = GMAPS_API_KEY.length;
 export default function Admin() {
   return (
     <div>
       <p>This is the admin page</p>
       <p>123</p>
-      <p>{GMAPS_API_KEY.length}</p>
+      <p>{alength}</p>
     </div>
   );
 }
